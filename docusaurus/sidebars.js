af23888082fd07b4546851e1ac41b362f562eca6
Ajeet Singh Raina, Docker Captain, ARM Innovator <ajeetraina@gmail.com>
2025-03-25 14:05:08 +0530

    Fix sidebar configuration to match actual document IDs

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -4,34 +4,59 @@ const sidebars = {
     {
       type: 'category',
       label: 'Introduction',
-      items: ['intro'],
+      items: ['intro', 'Kubernetes_Architecture', 'kube101'],
     },
     {
       type: 'category',
       label: 'Getting Started',
-      items: ['kube101', 'gke-setup', 'weave'],
+      items: ['gke-setup', 'weave'],
     },
     {
       type: 'category',
       label: 'Pods',
-      items: ['pods101/deploy-your-first-nginx-pod', 'pods101/labels-and-selectors'],
+      items: ['pods101/pods101_deploy-your-first-nginx-pod', 'pods101/pods101_FAQs'],
     },
     {
       type: 'category',
       label: 'ReplicaSets',
-      items: ['replicaset101/readme'],
+      items: ['ReplicationController101/ReplicationController101_readme'],
     },
     {
       type: 'category',
       label: 'Deployments',
-      items: ['Deployment101/readme'],
+      items: ['Deployment101/Deployment101_Blue-Green-Strategies', 'Deployment101/Deployment101_Rolling_Update'],
     },
     {
       type: 'category',
       label: 'Services',
-      items: ['Services101/readme'],
+      items: ['kubectl-for-docker', 'api'],
     },
-    // Add more categories as needed based on existing content
+    {
+      type: 'category',
+      label: 'ConfigMaps',
+      items: ['ConfigMaps101/ConfigMaps101_what-are-configmaps', 'secerts_configmaps101/secerts_configmaps101_secrets-configmaps'],
+    },
+    {
+      type: 'category',
+      label: 'Cloud Providers',
+      items: [
+        'AKS101/AKS101_what-is-aks',
+        'EKS101/EKS101_what-is-eks',
+        'GKE101/GKE101_what-is-gke',
+        'LKE101/LKE101_what-is-lke'
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Advanced Topics',
+      items: [
+        'Helm101/Helm101_what-is-helm',
+        'GitOps101/GitOps101_what-is-gitops',
+        'Security101/Security101_kubernetes-security',
+        'Logging101/Logging101_logging-intro',
+        'Observability101/Observability101_observability',
+      ],
+    },
   ],
 };
 
